07fdd1c23d2c24d0411edffed6511dc235721966
Jiéverson Maissiat <jieverson@craftbox.com.br>
2016-11-19 17:48:54 -0200

    code refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const fileinclude = require('gulp-file-include')
 
 gulp.task('default', function() {
-  gulp.src(['./src/index.html'])
+  gulp.src(['./src/*.html'])
     .pipe(fileinclude({
       prefix: '@',
       basepath: '@file'
